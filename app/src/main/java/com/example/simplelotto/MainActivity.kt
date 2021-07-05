e6aceb113a59000aef70b1f290b28bbba7ab0733
DongGu Kang <ehdrnsla3020@naver.com>
2021-07-05 23:13:01 +0900

    complete make webview

--- app/src/main/java/com/example/simplelotto/MainActivity.kt
+++ app/src/main/java/com/example/simplelotto/MainActivity.kt
@@ -2,8 +2,36 @@ package com.example.simplelotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.NumberPicker
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val clearButton : Button by lazy {
+        findViewById<Button>(R.id.btn_clear)
+    }
+    private val addButton : Button by lazy {
+        findViewById<Button>(R.id.btn_add)
+    }
+    private val runButton: Button by lazy {
+        findViewById<Button>(R.id.btn_run)
+    }
+    private val numberPicker : NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numPicker)
+    }
+
+    private val numberTextViewList : List<TextView> by lazy {
+        listOf<TextView>(
+            findViewById<TextView>(R.id.txt_1),
+            findViewById<TextView>(R.id.txt_2),
+            findViewById<TextView>(R.id.txt_3),
+            findViewById<TextView>(R.id.txt_4),
+            findViewById<TextView>(R.id.txt_5),
+            findViewById<TextView>(R.id.txt_6)
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
