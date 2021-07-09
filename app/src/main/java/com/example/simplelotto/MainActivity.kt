e124efa1cf4be6162f2166b546e0f1032ff35afc
DongGu Kang <ehdrnsla3020@naver.com>
2021-07-09 22:39:26 +0900

    complete simplelotto

--- app/src/main/java/com/example/simplelotto/MainActivity.kt
+++ app/src/main/java/com/example/simplelotto/MainActivity.kt
@@ -2,17 +2,16 @@ package com.example.simplelotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.NumberPicker
 import android.widget.TextView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
 
     private val clearButton : Button by lazy {
         findViewById<Button>(R.id.btn_clear)
-    }
-    private val addButton : Button by lazy {
-        findViewById<Button>(R.id.btn_add)
     }
     private val runButton: Button by lazy {
         findViewById<Button>(R.id.btn_run)
@@ -31,9 +30,61 @@ class MainActivity : AppCompatActivity() {
             findViewById<TextView>(R.id.txt_6)
         )
     }
+    private var didRun = false
+    private val pickNumberSet = hashSetOf<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numberPicker.minValue = 1
+        numberPicker.maxValue = 45
+
+        //Auto Number Picker
+        initRunButton()
+        ininClearButton()
     }
+
+    private fun initRunButton() {
+        runButton.setOnClickListener{
+            val list = getRandomNumber()
+            //if clear random number or not
+            didRun = true
+            list.forEachIndexed{index,number->
+                val textView = numberTextViewList[index]
+                textView.text = number.toString()
+                textView.isVisible = true
+            }
+            Log.d("MainActivity", list.toString())
+        }
+    }
+
+    private fun getRandomNumber():List<Int> {
+        val nummberList = mutableListOf<Int>().apply {
+            for(i in 1..45){
+                if (pickNumberSet.contains(i)){
+                    continue
+                }
+                this.add(i)
+            }
+        }
+        nummberList.shuffle()
+        val newList = pickNumberSet.toList()+nummberList.subList(0,6-pickNumberSet.size)
+        return newList.sorted()
+    }
+
+    private fun ininClearButton() {
+        clearButton.setOnClickListener{
+            pickNumberSet.clear()
+            numberTextViewList.forEach{
+                it.isVisible = false
+            }
+            val textView = numberTextViewList[0]
+            textView.text = "0"
+            textView.isVisible = true
+            didRun = false
+        }
+
+    }
+
 }
